3b1be71e03df6684aa5f040d2954f777c83a04a4
IamVulgar <39926540+IamVulgar@users.noreply.github.com>
2018-06-05 03:09:38 -0400

    Update channels.js

--- channels.js
+++ channels.js
@@ -9,7 +9,7 @@ $("#main").after(
 '</div>' +
 '<div class="scroll-images scrollable-x">' +
  '<a href="https://cytu.be/r/I_love_the_90s"><img src="http://ilovethe90stour.com/wp-content/themes/i_love_the_90s/images/logo.png" class="kek" /></a>' +
-'<a href="https://cytu.be/r/animatedshows"><img src="//s14.postimg.cc/arf6l60sh/Wea_Qe7_R.png" class="kek" /></a>' +
+'<a href="https://cytu.be/r/DragonBallAbridged"><img src="https://vignette.wikia.nocookie.net/teamfourstar/images/f/f1/Dragon_Ball_Z_Abridged_Logo.png/revision/latest?cb=20160129194643" class="kek" /></a>' +
  '<a href="https://cytu.be/r/seinfeldHD"><img src="//s14.postimg.cc/yv5y9gqz5/w3_WPXIa.png" class="kek" /></a>' +
  '<a href="https://cytu.be/r/billcartoon"><img src="//s14.postimg.cc/c6gr9whb5/Bdr_UNQZ.png" class="kek" /></a>' +
  '<a href="https://cytu.be/r/spookyshows"><img src="//s14.postimg.cc/twifuuuv5/2_LB9834.png" class="kek" /></a>' +
