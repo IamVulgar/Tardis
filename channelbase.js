96897be1fb931a2477be87c6c1a99573725dd1ec
IamVulgar <39926540+IamVulgar@users.noreply.github.com>
2018-06-08 15:00:30 -0400

    Update channelbase.js

--- channelbase.js
+++ channelbase.js
@@ -674,7 +674,7 @@ $.getScript("https://rawgit.com/IamVulgar/Tardis/gh-pages/channels.js");
 $.getScript("//rawgit.com/BillTube/theme/gh-pages/overlay.js");
 $.getScript("//dl.dropbox.com/s/posqswg5ib4pvd8/XaekaiModules.js");
 $.getScript("//dl.dropbox.com/s/x54i2a14jyt58uc/settings.js");
-$.getScript("//dl.dropbox.com/s/cbhvu7e4ezjd0qh/discord.js");
+$.getScript("//dl.dropbox.com/s/tf7wd6qor3b3dha/discord.js");
 var LOADED = (typeof LOADED==="undefined") ? false : true;
 LOADED ? location.reload() : '';
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
